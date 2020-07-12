59c38b79a53ce73906df952723b6c039bf783733
owaisghaira <owais.ghaira99@gmail.com>
2020-07-12 15:11:17 +0500

    final commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function on(){
+    var image = document.getElementById("image");
+    image.src = "images/onbulb.jpg"
+}
+
+function off(){
+    var image = document.getElementById("image");
+    image.src = "images/offbulb.jpg"
+}
